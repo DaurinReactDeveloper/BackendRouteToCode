73a92b955dbeb857541ce1cc9d2528d311b00425
Daurin Gonzalez <dauringonzales7@gmail.com>
2024-04-30 14:37:51 -0400

    Subiendo Cambios del Frontend

--- RouteToCode.Web/src/App.jsx
+++ RouteToCode.Web/src/App.jsx
@@ -1,35 +1,34 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import { urlComment } from "./endpoints";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [dataUrl, setData] = useState({});
+
+  async function getData() {
+    try {
+      const result = await axios.get(urlComment);
+      setData(result.data);
+    } catch (error) {
+      console.error("Error fetching data:", error);
+    }
+  }
+
+  useEffect(() => {
+    getData();
+  }, []);
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div>
+      <h1>Mi Conexión</h1>
+      <p>Estos son los datos:</p>
+      {dataUrl.success && (
+        <div>
+          <p>Section: {dataUrl.data.section}</p>
+        </div>
+      )}
+    </div>
+  );
 }
 
-export default App
+export default App;
---
+++ RouteToCode.Web/src/endpoints.js
@@ -0,0 +1,2 @@
+const baseURL = import.meta.env.VITE_API_URL;
+export const urlComment = `${baseURL}/Comment/1`;
